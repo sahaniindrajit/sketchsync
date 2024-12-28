3b2e6362da60799c03b88985f391ccd3e6e58eb4
Indrajit sahani <indrajit38mig@gmail.com>
2024-12-28 13:20:55 +0530

    Add arrow function

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,8 +1,10 @@
 import { Toolbar } from "@/components/toolBar"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { activeToolState } from "@/recoil/atoms/toolBarState"
-import React, { useCallback, useState, useRef, useEffect } from "react";
-import { Layer, Stage, Image as KonvaImage, Rect as KonvaRect } from "react-konva";
+import { activeToolState, fillColorState, strokeColorState, strokeWidthState } from "@/recoil/atoms/toolBarState"
+import { Arrow } from "@/types/shape.types";
+import { KonvaEventObject } from "konva/lib/Node";
+import React, { useCallback, useState, useRef } from "react";
+import { Layer, Stage, Image as KonvaImage, Rect as KonvaRect, Arrow as KonvaArrow } from "react-konva";
 import { useRecoilState } from "recoil"
 
 const downloadURI = (uri: string | undefined, name: string) => {
@@ -17,20 +19,18 @@ const downloadURI = (uri: string | undefined, name: string) => {
 // eslint-disable-next-line no-empty-pattern
 export const Board = React.memo(function Board({ }) {
     const [activeTool, setActiveTool] = useRecoilState(activeToolState);
+    const [strokeColor, setStrokeColor] = useRecoilState(strokeColorState);
+    const [strokeWidth, setStrokeWidth] = useRecoilState(strokeWidthState);
+    const [fillColor, setFillColor] = useRecoilState(fillColorState);
+    //const [x, setX] = useState(0);
+    //const [y, setY] = useState(0);
     const [image, setImage] = useState<HTMLImageElement>();
+    const [arrow, setArrow] = useState<Arrow>();
     const fileRef = useRef<HTMLInputElement>(null);
+    const isPaintRef = useRef(false)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const stageRef = useRef<any>(null)
 
-    const [dimensions, setDimensions] = useState({ width: window.innerWidth, height: window.innerHeight });
-
-    useEffect(() => {
-        const handleResize = () => {
-            setDimensions({ width: window.innerWidth, height: window.innerHeight });
-        };
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
 
     const handleDownload = useCallback(() => {
         const dataURI = stageRef?.current?.toDataURL({ devicePixelRatio })
@@ -53,53 +53,87 @@ export const Board = React.memo(function Board({ }) {
         fileRef?.current?.click();
     }, []);
 
-    switch (activeTool) {
-        case 'image':
-            break;
-        case 'download':
-            handleDownload();
-            break;
-    }
+    const onStageMouseDown = useCallback((e: KonvaEventObject<MouseEvent>) => {
+        if (activeTool === 'select') return;
+        isPaintRef.current = true;
+        const stage = stageRef.current;
+        const pos = stage?.getPointerPosition();
+        const x = pos?.x || 0
+        const y = pos?.y || 0
+        switch (activeTool) {
+
+            case 'arrow':
+                setArrow({ id: '1', color: { fillColor }, strokeColor: { strokeColor }, strokeWidth: { strokeWidth }, points: [x, y, x, y] })
+        }
+    }, [activeTool, fillColor, strokeColor, strokeWidth]);
+    const onStageMouseUp = useCallback(() => { }, []);
+    const onStageMouseMove = useCallback(() => { }, []);
+
+
 
     return (
-        <div>
-            <TooltipProvider>
-                <Toolbar
-                    selectedTool={activeTool}
-                    onToolSelect={setActiveTool}
-                    handleUpload={handleUpload}
-                    handleUploadButtonClick={onUploadButtonClick}
-                    fileInputRef={fileRef}
-                />
-                <h1>
-                    {activeTool}
-                </h1>
-            </TooltipProvider>
-            <Stage width={dimensions.width} height={dimensions.height} ref={stageRef}>
-                <Layer>
-                    <KonvaRect
-                        x={0}
-                        y={0}
-                        height={window.innerHeight / 2}
-                        width={window.innerHeight / 2}
-                        fill="white"
-                        id="bg" />
+        <>
+            <div>
 
-                    {
-                        image && (
-                            <KonvaImage image={image}
-                                x={0}
-                                y={0}
-                                height={window.innerHeight / 2}
-                                width={window.innerHeight / 2}>
+                <TooltipProvider>
+                    <Toolbar
+                        selectedTool={activeTool}
+                        onToolSelect={setActiveTool}
+                        handleUpload={handleUpload}
+                        handleUploadButtonClick={onUploadButtonClick}
+                        handelDownload={handleDownload}
+                        fileInputRef={fileRef}
+                    />
+                    <h1>
+                        {activeTool}
+                    </h1>
+                </TooltipProvider>
 
-                            </KonvaImage>
-                        )
-                    }
-                </Layer>
+                <Stage
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    ref={stageRef}
+                    onMouseDown={onStageMouseDown}
+                    onMouseMove={onStageMouseMove}
+                    onMouseUp={onStageMouseUp}
+                >
+                    <Layer>
+                        <KonvaRect
+                            x={0}
+                            y={0}
+                            height={window.innerHeight}
+                            width={window.innerHeight}
+                            fill="white"
+                            id="bg"
+                        />
+                        {
+                            image && (
+                                <KonvaImage image={image}
+                                    x={0}
+                                    y={0}
+                                    height={window.innerHeight / 2}
+                                    width={window.innerHeight / 2}>
 
-            </Stage>
+                                </KonvaImage>
+                            )
+                        }
+                        {
+                            arrow && (
+                                <KonvaArrow
+                                    key={arrow.id}
+                                    id={arrow.id}
+                                    points={arrow.points}
+                                    fill={arrow.color.fillColor}
+                                    stroke={arrow.strokeColor.strokeColor}
+                                    strokeWidth={arrow.strokeWidth.strokeWidth}
+                                />
+                            )
+                        }
+                    </Layer>
 
-        </div>
+                </Stage>
+
+            </div>
+        </>
     )
 });

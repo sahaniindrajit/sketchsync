68ef756e7355c536c71e7ebb9177dc4fd61c764f
Indrajit sahani <indrajit38mig@gmail.com>
2024-12-28 13:18:44 +0530

    Add shape type

---
+++ src/types/shape.types.ts
@@ -0,0 +1,29 @@
+export type Shape = {
+    id: string;
+    color: { fillColor: string };
+    x?: number;
+    y?: number;
+};
+
+export type Rectangle = Shape & {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+};
+
+export type Circle = Shape & {
+    radius: number;
+    x: number;
+    y: number;
+};
+
+export type Scribble = Shape & {
+    points: number[];
+};
+
+export type Arrow = Shape & {
+    points: [number, number, number, number];
+    strokeWidth: { strokeWidth: number }
+    strokeColor: { strokeColor: string }
+};
